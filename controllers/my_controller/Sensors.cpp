459737bb8285dbf4629b6540a230a58c3a1427fa
daniel347 <daniel.siromani@gmail.com>
2021-03-01 17:53:30 +0000

    Added sensor header and visual studio project for my_controller

---
+++ controllers/my_controller/Sensors.cpp
@@ -0,0 +1,38 @@
+#include <webots/Robot.hpp>
+#include <webots/DistanceSensor.hpp>
+#include <webots/LightSensor.hpp>
+
+#include <Sensors.hpp>
+
+using namespace std;
+
+/**
+* Initialises and enables the distance sensor. By default the sampling 
+* period is equal to the simualion timestep.
+*
+* @param robot (Robot*) a pointer to the robot object
+* @param name (const char*) the name of the distance sensor
+* @returns ds (DistanceSensor*) a pointer to the DistanceSensor object
+*/
+
+DistanceSensor* initDistanceSensor(Robot* robot, const char* name) {
+  DistanceSensor* ds = robot->getDistanceSensor(name);
+  ds->enable(robot->getBasicTimeStep());  // I dont like this
+  return ds
+}
+
+/**
+* Simulates making a distance measurement with the 
+* ultrasonic distance sensor
+* 
+* @param ds (DistanceSensor*) the sensor to read
+* @returns distance (int) the distance measurement in millimeters
+*/
+
+int getDistanceMeasurement(DistanceSensor* ds) {
+  int measurement = ds->getValue();
+  return ds->getValue();
+}
+
+
+
---
+++ controllers/my_controller/Sensors.hpp
@@ -0,0 +1,6 @@
+#include <webots/Robot.hpp>
+#include <webots/DistanceSensor.hpp>
+#include <webots/LightSensor.hpp>
+
+DistanceSensor* initDistanceSensor(Robot* robot, const char* name);
+int getDistanceMeasurement(DistanceSensor* ds);
