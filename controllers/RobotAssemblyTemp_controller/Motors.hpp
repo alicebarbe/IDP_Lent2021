c2f5256fdc686595c85797b23c1ca78311a4633a
daniel347 <daniel.siromani@gmail.com>
2021-03-20 21:03:10 +0000

    Removed pointless controllers and renamed world

--- controllers/RobotAssemblyTemp_controller/Motors.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <webots/Robot.hpp>
-#include <webots/Motor.hpp>
-#include <tuple>
-
-std::tuple<webots::Motor*, webots::Motor*, webots::Motor*, webots::Motor*> initMotors(webots::Robot* robot, const char* left_motor, const char* right_motor, const char* trap_motor, const char* flipper_motor);
-std::tuple<double, double, double, double> getMotorVelocity(std::tuple<webots::Motor*, webots::Motor*, webots::Motor*, webots::Motor*> motors);
-void setMotorVelocity(std::tuple<webots::Motor*, webots::Motor*, webots::Motor*, webots::Motor*> motors, std::tuple<double, double, double, double>);
